74c1a0bef495d4bdf8cecee5038ac67e7a191360
Hector <hlu0723@student.skelleftea.se>
2025-11-05 09:44:37 +0100

    functioner påbörjad

---
+++ app.ts
@@ -0,0 +1,19 @@
+/*
+function summa(Tal1: number, Tal2: number, Tal3:number){
+return Tal1+Tal2+Tal3
+}
+write (summa(1, 7, 9))
+*/
+/*
+function dist(Tal1: number, Tal2: number){
+    if(Tal1>Tal2)
+        return Tal1-Tal2
+    if(Tal1<Tal2)
+        return Tal2-Tal1
+}
+write(dist(67, 10))
+*/
+function dist(x1, y1, x2, y2){
+    
+}
+write(dist(2, 4, 14, 25))
